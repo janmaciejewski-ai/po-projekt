c41ffbf0343198031684d8cf09c95786e87c07ce
Jan Maciejewski <jan.maciejewski@local>
2026-01-11 14:33:41 +0100

    api koniec

--- app/app.py
+++ app/app.py
@@ -30,6 +30,13 @@ def validate_user_data(data, partial=False):
 def get_users():
     return jsonify(storage.get_all()), 200
 
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = storage.get_by_id(user_id)
+    if not user:
+        abort(404)
+    return jsonify(user), 200
+
 @app.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json(silent=True)
@@ -39,6 +46,37 @@ def create_user():
     
     user = storage.add(data['name'], data['lastname'])
     return jsonify(user), 201
+
+@app.route('/users/<int:user_id>', methods=['PATCH'])
+def patch_user(user_id):
+    user = storage.get_by_id(user_id)
+    if not user:
+        abort(400)
+
+    data = request.get_json(silent=True)
+    error = validate_user_data(data, partial=True)
+    if error:
+        abort(400)
+
+    storage.update(user_id, data)
+    return '', 204
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def put_user(user_id):
+    data = request.get_json(silent=True)
+    error = validate_user_data(data, partial=False)
+    if error:
+        abort(400)
+        
+    storage.put(user_id, data['name'], data['lastname'])
+    return '', 204
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    deleted = storage.delete(user_id)
+    if not deleted:
+        abort(400)
+    return '', 204
 
 if __name__ == '__main__':
     app.run(debug=True)
